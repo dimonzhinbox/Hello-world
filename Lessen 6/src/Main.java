8a542c04b45dbf0a6f1baa3e4d5d2c40729e1d0f
dimonzhinbox <dimon.zh1984@gmail.com>
2024-10-03 17:12:16 +0300

    Прописал условия

---
+++ Lessen 6/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       // System.out.println(5!=3);
+        // <
+        // >
+        // ==
+        // <=
+        // >=
+        // !=
+        System.out.println("Первая: ");
+        int a = new Scanner(System.in).nextInt();
+        if (a==5){
+            System.out.println("55");
+            a=6;
+            if (a==6){
+                System.out.println("66");
+                if (a==7){
+                    System.out.println("77");
+                }
+            }
+        }
+//        System.out.println("Вторая: ");
+//        int b = new Scanner(System.in).nextInt();
+//
+//        if (a>5 || b<5){
+//            System.out.println("HW");
+//        }
+        //логическое - и = &&
+        //логическое - или = ||
+//        if (a<5 && b>5){
+//            System.out.println("GW");
+//        }
+
+
+
+    }
+}
